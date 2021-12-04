f2734c293c33b0a48b4d67581147ccc6d7775980
Pablo <torrentep@gmail.com>
2021-12-04 23:14:40 +0100

    Web Forms With Flask - Python and Flask #4

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+subscribers = []
 
 @app.route('/')
 def index():
@@ -11,3 +13,17 @@ def about():
     names = ['John', 'Mary', 'Wes','Annie','Suzy']
 
     return render_template('about.html', names = names) # passing variable to a web page
+
+@app.route('/subscribe')
+def subscribe():
+    
+    return render_template('subscribe.html')
+
+@app.route('/form', methods=["POST"]) # estamos posteando info a form.html
+def form():
+    
+    first_name = request.form.get("first_name") #recibe las variables name del formulario
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    subscribers.append(f"{first_name} {last_name} {email}")
+    return render_template('form.html', subscribers = subscribers)
