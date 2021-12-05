1a5a59e2694999987576cfdcc35e1e7d10649f68
Pablo <torrentep@gmail.com>
2021-12-05 19:06:05 +0100

    Handling Web Form Errors - Python and Flask #5

--- app.py
+++ app.py
@@ -25,5 +25,10 @@ def form():
     first_name = request.form.get("first_name") #recibe las variables name del formulario
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+    
+    if not first_name or not last_name or not email:
+        error_statement = "All form fields required ..."
+        return render_template( "subscribe.html", error_statement = error_statement, 
+                               first_name=first_name, last_name=last_name, email=email)
     subscribers.append(f"{first_name} {last_name} {email}")
     return render_template('form.html', subscribers = subscribers)
